88d5aa999aaf9cf497dd4504501e177d33b05675
PasinduJayasinghe <thisarajayasinghe07@gmail.com>
2025-01-26 11:16:39 +0530

    Hangman game

---
+++ CreateDBFromScratch/CreateDBFromScratch/Models/SchoolContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CreateDBFromScratch.Models
+{
+    public class SchoolContext:DbContext
+    {
+        public DbSet<Students> Students { get; set; }
+
+        public SchoolContext(DbContextOptions options) :base(options) 
+        {
+            
+        }
+    }
+}
